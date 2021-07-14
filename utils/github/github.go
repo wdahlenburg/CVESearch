c5a3297cfe55dfed7f0dc55bfbe570bd907c85ee
Wyatt Dahlenburg <wddahlenburg@gmail.com>
2021-07-14 10:15:27 -0500

    Search projectdiscovery/nuclei-templates once a Github key is set up

--- utils/github/github.go
+++ utils/github/github.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"strings"
 
 	"github.com/google/go-github/github"
 	"golang.org/x/oauth2"
@@ -16,9 +17,6 @@ func New() *GitHub {
 }
 
 func (g *GitHub) Start(cve string, gitKey string, verbose bool) {
-	var (
-		results []string
-	)
 	if gitKey == "" {
 		if verbose {
 			log.Printf("GITHUB_KEY environment variable isn't set")
@@ -33,6 +31,14 @@ func (g *GitHub) Start(cve string, gitKey string, verbose bool) {
 	tc := oauth2.NewClient(ctx, ts)
 	client := github.NewClient(tc)
 
+	g.startGithub(ctx, client, cve, verbose)
+	g.startNuclei(ctx, client, cve, verbose)
+}
+
+func (g *GitHub) startGithub(ctx context.Context, client *github.Client, cve string, verbose bool) {
+	var (
+		results []string
+	)
 	repos, _, err := client.Search.Repositories(ctx, cve, nil)
 	if err != nil {
 		if verbose {
@@ -41,21 +47,54 @@ func (g *GitHub) Start(cve string, gitKey string, verbose bool) {
 		return
 	}
 
-	if verbose {
-		log.Printf("Found %d repositories in GitHub\n", len(repos.Repositories))
-	}
 	for i := 0; i < len(repos.Repositories); i++ {
 		results = append(results, *repos.Repositories[i].HTMLURL)
+	}
+
+	if verbose {
+		if len(results) == 0 {
+			log.Println("No results found in GitHub")
+		} else {
+			log.Printf("Found %d repositories in GitHub\n", len(repos.Repositories))
+		}
+	}
+
+	g.prettyPrint(results)
+}
+
+func (g *GitHub) startNuclei(ctx context.Context, client *github.Client, cve string, verbose bool) {
+	var (
+		results []string
+	)
+	// Query is: "CVE-20XX-YYYY in:file language:yaml repo:projectdiscovery/nuclei-templates"
+	query := fmt.Sprintf("%s in:file language:yaml repo:projectdiscovery/nuclei-templates", cve)
+	files, _, err := client.Search.Code(ctx, query, nil)
+	if err != nil {
+		if verbose {
+			log.Printf(err.Error())
+		}
+		return
+	}
+
+	for i := 0; i < *files.Total; i++ {
+		// Do a check to make sure it's in the 'cves' folder
+		if strings.Contains(*files.CodeResults[i].HTMLURL, "cves/") {
+			results = append(results, *files.CodeResults[i].HTMLURL)
+		}
+	}
+
+	if verbose {
+		if len(results) == 0 {
+			log.Println("No results found in Nuclei-Templates")
+		} else {
+			log.Printf("Found %d file(s) in Nuclei-Templates\n", len(results))
+		}
 	}
 
 	g.prettyPrint(results)
 }
 
 func (g *GitHub) prettyPrint(results []string) {
-	if len(results) == 0 {
-		log.Println("No results found in GitHub")
-		return
-	}
 	for _, result := range results {
 		fmt.Printf("%s\n", result)
 	}
